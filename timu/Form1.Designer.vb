19dea25bd622eb2168eae40968ae12213658b2a9
Glagle <220153591@seu.edu.cn>
2017-06-21 15:14:27 +0800

    vb

---
+++ timu/Form1.Designer.vb
@@ -0,0 +1,204 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.RichTextBox2 = New System.Windows.Forms.RichTextBox()
+        Me.RichTextBox3 = New System.Windows.Forms.RichTextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(209, 41)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(126, 42)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "点我抽人"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(335, 206)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(0, 12)
+        Me.Label1.TabIndex = 8
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("宋体", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.Label4.Location = New System.Drawing.Point(430, 21)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(80, 16)
+        Me.Label4.TabIndex = 11
+        Me.Label4.Text = "幸运的人:"
+        '
+        'RichTextBox2
+        '
+        Me.RichTextBox2.Font = New System.Drawing.Font("宋体", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.RichTextBox2.Location = New System.Drawing.Point(421, 221)
+        Me.RichTextBox2.Name = "RichTextBox2"
+        Me.RichTextBox2.Size = New System.Drawing.Size(332, 394)
+        Me.RichTextBox2.TabIndex = 14
+        Me.RichTextBox2.Text = ""
+        '
+        'RichTextBox3
+        '
+        Me.RichTextBox3.Font = New System.Drawing.Font("宋体", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.RichTextBox3.Location = New System.Drawing.Point(821, 221)
+        Me.RichTextBox3.Name = "RichTextBox3"
+        Me.RichTextBox3.Size = New System.Drawing.Size(332, 394)
+        Me.RichTextBox3.TabIndex = 15
+        Me.RichTextBox3.Text = ""
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("宋体", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(433, 45)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(160, 38)
+        Me.TextBox1.TabIndex = 16
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Font = New System.Drawing.Font("宋体", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.RichTextBox1.Location = New System.Drawing.Point(27, 221)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(321, 394)
+        Me.RichTextBox1.TabIndex = 17
+        Me.RichTextBox1.Text = ""
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("宋体", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.Button2.Location = New System.Drawing.Point(134, 162)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(95, 41)
+        Me.Button2.TabIndex = 18
+        Me.Button2.Text = "阿诺新题"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Font = New System.Drawing.Font("宋体", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.Button3.Location = New System.Drawing.Point(532, 162)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(89, 40)
+        Me.Button3.TabIndex = 19
+        Me.Button3.Text = "安素泰题"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Font = New System.Drawing.Font("宋体", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.Button4.Location = New System.Drawing.Point(937, 163)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(92, 40)
+        Me.Button4.TabIndex = 20
+        Me.Button4.Text = "法玛新题"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 500
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Font = New System.Drawing.Font("宋体", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.TextBox2.Location = New System.Drawing.Point(929, 40)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 38)
+        Me.TextBox2.TabIndex = 21
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("宋体", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.Label2.Location = New System.Drawing.Point(926, 21)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(64, 16)
+        Me.Label2.TabIndex = 22
+        Me.Label2.Text = "30s计时"
+        '
+        'Button5
+        '
+        Me.Button5.Font = New System.Drawing.Font("宋体", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.Button5.Location = New System.Drawing.Point(740, 40)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(113, 41)
+        Me.Button5.TabIndex = 23
+        Me.Button5.Text = "停止计时"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1197, 639)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.RichTextBox3)
+        Me.Controls.Add(Me.RichTextBox2)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "抽题软件"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents RichTextBox2 As RichTextBox
+    Friend WithEvents RichTextBox3 As RichTextBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button5 As Button
+End Class
---
+++ timu/Form1.vb
@@ -0,0 +1,155 @@
+﻿
+Public Class Form1
+    Dim ID(40) As Integer
+    Dim time_count As Single = 30
+    Dim count As Integer = 41       '总人数
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        For i = 0 To 40
+            ID(i) = i
+        Next
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim AllText As String
+        Dim a_CHT() As String
+
+        Dim person As Integer
+
+
+        Randomize()                              '必要代码，不然的话每次随机生成都是一样的数
+        person = Int(Rnd() * (count - 1 - 0 + 1)) + 0   '其中m>n生成n~m间（包括n，m）的随机整数
+
+
+        AllText = My.Computer.FileSystem.ReadAllText(Application.StartupPath() & "\data\name.txt ")
+        a_CHT = AllText.Split(vbCrLf)     '将文本按行分割成字符串数组a_CHT（）
+
+
+        '显示人名及题目内容
+        If count > 0 Then
+            TextBox1.Text = a_CHT(ID(person))
+        End If
+
+        If count = 0 Then
+            TextBox1.Text = "GAME 0VER"
+
+        End If
+
+        If count > 0 Then
+
+            '抽到的人不再抽
+            Dim tmp As Integer = 0
+            tmp = ID(person)
+            ID(person) = ID(count - 1)
+            ID(count - 1) = tmp
+            count = count - 1
+
+        End If
+
+        time_count = 30
+        TextBox2.Text = Format(time_count, "###0.0")
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Label4_Click(sender As Object, e As EventArgs) Handles Label4.Click
+
+    End Sub
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        Dim AllText_TI_1 As String
+        Dim a_CHT_TI_1() As String
+
+        Dim TI_1 As Integer
+        Randomize()                              '必要代码，不然的话每次随机生成都是一样的数
+        TI_1 = Int(Rnd() * (55 - 0 + 1)) + 0   '其中m>n生成n~m间（包括n，m）的随机整数
+
+        AllText_TI_1 = My.Computer.FileSystem.ReadAllText(Application.StartupPath() & "\data\TI_1.txt ")
+        a_CHT_TI_1 = AllText_TI_1.Split(vbCrLf)     '将文本按行分割成字符串数组a_CHT（）
+
+        RichTextBox1.Text = Replace(Replace(a_CHT_TI_1(TI_1), vbCr, ""), vbLf, "")
+
+        RichTextBox2.Text = ""
+        RichTextBox3.Text = ""
+
+        Timer1.Interval = 500
+        Timer1.Enabled = True
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim AllText_TI_2 As String
+        Dim a_CHT_TI_2() As String
+
+        Dim TI_2 As Integer
+        Randomize()                              '必要代码，不然的话每次随机生成都是一样的数
+        TI_2 = Int(Rnd() * (34 - 0 + 1)) + 0   '其中m>n生成n~m间（包括n，m）的随机整数
+
+        AllText_TI_2 = My.Computer.FileSystem.ReadAllText(Application.StartupPath() & "\data\TI_2.txt ")
+        a_CHT_TI_2 = AllText_TI_2.Split(vbCrLf)     '将文本按行分割成字符串数组a_CHT（）
+
+        RichTextBox2.Text = Replace(Replace(a_CHT_TI_2(TI_2), vbCr, ""), vbLf, "")
+
+        RichTextBox1.Text = ""
+        RichTextBox3.Text = ""
+
+        Timer1.Interval = 500
+        Timer1.Enabled = True
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+
+        Dim AllText_TI_3 As String
+        Dim a_CHT_TI_3() As String
+
+        Dim TI_3 As Integer
+        Randomize()                              '必要代码，不然的话每次随机生成都是一样的数
+        TI_3 = Int(Rnd() * (77 - 0 + 1)) + 0   '其中m>n生成n~m间（包括n，m）的随机整数
+
+        AllText_TI_3 = My.Computer.FileSystem.ReadAllText(Application.StartupPath() & "\data\TI_3.txt ")
+        a_CHT_TI_3 = AllText_TI_3.Split(vbCrLf)     '将文本按行分割成字符串数组a_CHT（）
+
+        RichTextBox3.Text = Replace(Replace(a_CHT_TI_3(TI_3), vbCr, ""), vbLf, "")
+
+        RichTextBox1.Text = ""
+        RichTextBox2.Text = ""
+
+        Timer1.Interval = 500
+        Timer1.Enabled = True
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+        time_count = time_count - 0.5
+        TextBox2.Text = Format(time_count, "###0.0")
+
+        If (time_count = 0) Then
+            Timer1.Enabled = False
+        End If
+
+
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+
+
+        Timer1.Enabled = False
+
+
+    End Sub
+End Class
